b38577ff19414105fc20e28fce24ef25183e99e7
Jelani.Joseph <jelani.joseph1@marist.edu>
2020-02-05 18:06:27 -0500

    code for number 3

---
+++ labs/1/newpi.java
@@ -0,0 +1,7 @@
+public class newpi {
+    public static void main(String[] args) {
+        double pi = 4 * ((1.0) - (1 / 3.) + (1 / 5.) - (1 / 7.) + (1 / 9.) - (1 / 11.) + (1 / 13.));
+
+        System.out.println("Pi + (1/13.) would be : " + pi);
+    }
+}
