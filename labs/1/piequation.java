042dfe8970df6cb8c7a5881c0f443885138b7fbc
Jelani.Joseph <jelani.joseph1@marist.edu>
2020-02-05 18:06:53 -0500

    code for number 2

---
+++ labs/1/piequation.java
@@ -0,0 +1,7 @@
+public class piequation {
+    public static void main(String[] args){
+        double pi = 4 * ((1.0) - (1/3.) + (1/5.) - (1/7.) + (1/9.) - ( 1/11.));
+
+        System.out.println("Pi is normally equal to: " + pi);
+    }
+}
