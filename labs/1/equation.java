5f6e7861a261c2f722b609a62f038674e68e64d9
Juan Arias <juan.arias@marist.edu>
2020-02-11 22:27:17 -0500

    Feedback

--- labs/1/equation.java
+++ labs/1/equation.java
@@ -1,6 +1,6 @@
 public class equation {
     public static void main(String[] args) {
-        double variable = (15.4 * 6.5 - 4.3 * 3) / 46.3 - 3.5;
+        double variable = (15.4 * 6.5 - 4.3 * 3) / (46.3 - 3.5); // JA: missing one set of parenthesis
         System.out.println("the answer for 15.4 * 6.5 - 4.3(3) / 46.3 - 3.5 is: " + variable);
     }
 }
--- labs/1/newpi.java
+++ labs/1/newpi.java
@@ -1,7 +1,7 @@
 public class newpi {
     public static void main(String[] args) {
         // same as pi, but this time there is a + 1/13
-        double pi = 4 * ((1.0) - (1 / 3.) + (1 / 5.) - (1 / 7.) + (1 / 9.) - (1 / 11.) + (1 / 13.));
+        double pi = 4 * ((1.0) - (1 / 3.) + (1 / 5.) - (1 / 7.) + (1 / 9.) - (1 / 11.) + (1 / 13.)); // JA: You needed both calculations
         System.out.println("Pi + (1/13.) would be : " + pi);
     }
 }
