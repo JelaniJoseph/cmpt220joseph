ec81fe0363bab55b94d29e1f7b80a36d56885fb9
Jelani.Joseph <jelani.joseph1@marist.edu>
2020-02-21 08:55:33 -0500

    question 2

---
+++ labs/3/largestprime.java
@@ -0,0 +1,19 @@
+public class largestprime{
+    public static void main(String[] args){
+        int n;
+
+        for(n=14999;n>0;n--){
+            int c=0;
+            for(int i=1;i<=n;i++){
+                if(n% i ==0){
+                    c++;
+                }
+            }
+            if (c == 2){
+                break;
+            }
+        } 
+        System.out.println("Largest n which is prime as well as less than 15000 is "+n);
+        
+        }
+}
